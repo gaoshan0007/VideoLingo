6a888429a4af7477d15e6fbf82b12bffa523c91d
gaoshan <le640@qq.com>
2024-11-10 15:11:07 +0800

    fix bug

--- core/ask_gpt.py
+++ core/ask_gpt.py
@@ -69,8 +69,7 @@ def ask_gpt(prompt, response_json=True, valid_def=None, log_title='default', ski
                 model=api_set["model"],
                 messages=messages,
                 response_format=response_format,
-                timeout=150, #! set timeout
-                response_format="json"
+                timeout=150 #! set timeout                
             )
             
             if response_json:
